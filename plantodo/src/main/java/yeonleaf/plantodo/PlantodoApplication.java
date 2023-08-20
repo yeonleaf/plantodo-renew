bb935eb155df829abdf07fdaff9fe28bc67d78db
yeonleaf <91470133+yeonleaf@users.noreply.github.com>
2023-08-20 18:17:46 +0900

    build: add openAPIDefinition (https url)

--- plantodo/src/main/java/yeonleaf/plantodo/PlantodoApplication.java
+++ plantodo/src/main/java/yeonleaf/plantodo/PlantodoApplication.java
@@ -1,14 +1,16 @@
 package yeonleaf.plantodo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://plantodo.site/", description = "Default Server URL")})
 @SpringBootApplication
 public class PlantodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlantodoApplication.class, args);
 	}
-
 
 }

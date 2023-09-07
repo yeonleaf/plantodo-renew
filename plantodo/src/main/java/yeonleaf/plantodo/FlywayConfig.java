7db0a715d7e6cb0a941dbcb3e79cc56608f2e9c7
yeonleaf <91470133+yeonleaf@users.noreply.github.com>
2023-09-07 15:52:26 +0900

    build: add ConditionalOnProperty to FlywayConfig to prevent executing flyway in test env

--- plantodo/src/main/java/yeonleaf/plantodo/FlywayConfig.java
+++ plantodo/src/main/java/yeonleaf/plantodo/FlywayConfig.java
@@ -1,6 +1,7 @@
 package yeonleaf.plantodo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ public class FlywayConfig {
     private boolean flywayEnabled;
 
     @Bean
+    @ConditionalOnProperty(name = "flyway.enabled", havingValue = "true")
     public FlywayMigrationStrategy repairMigrationStrategy() {
         return flyway -> {
             if (flywayEnabled) {

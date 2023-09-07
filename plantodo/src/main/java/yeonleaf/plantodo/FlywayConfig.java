70d979c4d9870dab7ac67e4176b19324b74859f9
yeonleaf <91470133+yeonleaf@users.noreply.github.com>
2023-09-07 15:40:07 +0900

    build: edit FlywayConfig to prevent it executed in test env

--- plantodo/src/main/java/yeonleaf/plantodo/FlywayConfig.java
+++ plantodo/src/main/java/yeonleaf/plantodo/FlywayConfig.java
@@ -1,5 +1,6 @@
 package yeonleaf.plantodo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,11 +8,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FlywayConfig {
 
+    @Value("${flyway.enabled}")
+    private boolean flywayEnabled;
+
     @Bean
     public FlywayMigrationStrategy repairMigrationStrategy() {
         return flyway -> {
-            flyway.repair();
-            flyway.migrate();
+            if (flywayEnabled) {
+                flyway.repair();
+                flyway.migrate();
+            }
         };
     }
 

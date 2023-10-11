70cd49b64a12ced9c0e51e0ffedf2ed3b3670490
yeonleaf <91470133+yeonleaf@users.noreply.github.com>
2023-10-11 11:21:14 +0900

    feat: remove flyway dependency

--- plantodo/src/main/java/yeonleaf/plantodo/converter/RepOutToInConverter.java
+++ plantodo/src/main/java/yeonleaf/plantodo/converter/RepOutToInConverter.java
@@ -7,6 +7,9 @@ import yeonleaf.plantodo.domain.Repetition;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * target description: {@link Repetition}을 {@link RepInputDto}로 변환하는 Converter
+ */
 public class RepOutToInConverter implements Converter<Repetition, RepInputDto> {
 
     private static final String[] koreanDates = {"월", "화", "수", "목", "금", "토", "일"};
@@ -32,6 +35,11 @@ public class RepOutToInConverter implements Converter<Repetition, RepInputDto> {
 
     }
 
+    /**
+     * repOption=3인 경우에만 사용하는 타입 변환용 보조 메소드
+     * @param repValue String 형태의 비트 문자열 (ex 1010100)
+     * @return 요일이 들어 있는 리스트 (ex 월, 수, 금)
+     */
     private List<String> parseToList(String repValue) {
         List<String> repList = new ArrayList<>();
         for (int i = 0; i < 7; i++) {

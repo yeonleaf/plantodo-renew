1f51894b8c41eccf4c5225ea60bbbdaddaa82578
yeonleaf <91470133+yeonleaf@users.noreply.github.com>
2024-03-24 09:36:26 +0900

    Revert "feat: apply mysql replication using docker"
    This reverts commit ea934a501b61abc4b05e3534720c9748e47d6947.

--- plantodo/slave/slave.sh
+++ plantodo/slave/slave.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
-mysql -u root -p1234 -e "create user 'repl'@'%' identified with mysql_native_password by '1234';"
-mysql -u root -p1234 -e "grant replication slave, replication client on *.* to 'repl'@'%';"
-master_log_file=`mysql -u root -h master -P 3306 -p1234 -e "show master status\G" | grep mysql-bin`
+mysql -u root -h 172.19.0.12 -p1234 -e "create user 'repl'@'%' identified with mysql_native_password by '1234';"
+mysql -u root -h 172.19.0.12 -p1234 -e "grant replication slave, replication client on *.* to 'repl'@'%';"
+
+master_log_file=`mysql -u root -h 172.19.0.11 -p1234 -e "show master status\G" | grep mysql-bin`
 echo ${master_log_file}
 re="[a-z]*-bin.[0-9]*"
 if [[ ${master_log_file} =~ $re ]];then
     master_log_file=${BASH_REMATCH[0]}
 fi
 
-master_log_pos=`mysql -uroot -h master -P 3306 -p1234 -e "show master status\G" | grep Position`
+master_log_pos=`mysql -uroot -h 172.19.0.11 -p1234 -e "show master status\G" | grep Position`
 echo ${master_log_pos}
 re="[0-9]+"
 if [[ ${master_log_pos} =~ $re ]];then
@@ -17,6 +18,7 @@ if [[ ${master_log_pos} =~ $re ]];then
 fi
 
 stat="CHANGE MASTER TO MASTER_HOST='master',MASTER_USER='repl',MASTER_PASSWORD='1234',MASTER_LOG_FILE='${master_log_file}',MASTER_LOG_POS=${master_log_pos};"
-mysql -uroot -p1234 -e "${stat}"
-mysql -uroot -p1234 -e "start slave"
+mysql -uroot -h 172.19.0.12 -p1234 -e "${stat}"
+mysql -uroot -h 172.19.0.12 -p1234 -e "start slave"
+/bin/bash
 exit;
